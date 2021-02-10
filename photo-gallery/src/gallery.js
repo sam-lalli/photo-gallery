1ff55225dc703973955e9b24f338880292691502
Sam Lalli <2sam.lalli@gmail.com>
2021-02-09 17:59:23 -0700

    data seeded and rendered

---
+++ photo-gallery/src/gallery.js
@@ -0,0 +1,133 @@
+import week1 from './assets/IMG-1890.jpg'
+import week2 from './assets/IMG-1891.jpg'
+import week3 from './assets/IMG-1892.jpg'
+import week4 from './assets/IMG-1893.jpg'
+import week5 from './assets/IMG-1894.jpg'
+import week6 from './assets/IMG-1895.jpg'
+import week7 from './assets/IMG-1896.jpg'
+import week8 from './assets/IMG-1897.jpg'
+import week9 from './assets/IMG-1898.jpg'
+import week10 from './assets/IMG-1899.jpg'
+import week11 from './assets/IMG-1900.jpg'
+import week12 from './assets/IMG-1901.jpg'
+import week13 from './assets/IMG-1902.jpg'
+import week14 from './assets/IMG-1903.jpg'
+import week15 from './assets/IMG-1904.jpg'
+import week16 from './assets/IMG-1905.jpg'
+import week17 from './assets/IMG-1906.jpg'
+import week18 from './assets/IMG-1910.PNG'
+
+
+
+
+export const gallery = [
+    {
+        id: 0,
+        img: week1,
+        title: 'The Dawn Of An Era',
+        week: 'Week 1'
+    },
+    {
+        id: 1,
+        img: week2,
+        title: 'Life is Delicate',
+        week: 'Week 2'
+    },
+    {
+        id: 2,
+        img: week3,
+        title: 'Enjoy The Finer Things In Life',
+        week: 'Week 3'
+    },
+    {
+        id: 3,
+        img: week4,
+        title: 'She Caught The Waves Just Thumbing Through My Braids',
+        week: 'Week 4'
+    },
+    {
+        id: 4,
+        img: week5,
+        title: 'I Really Like This One',
+        week: 'Week 5'
+    },
+    {
+        id: 5,
+        img: week6,
+        title: 'I Have An Above Average Of Money',
+        week: 'Week 6'
+    },
+    {
+        id: 6,
+        img: week7,
+        title: 'You Always Go Where You Are Looking',
+        week: 'Week 7'
+    },
+    {
+        id: 7,
+        img: week8,
+        title: 'A Different Perspective',
+        week: 'Week 8'
+    },
+    {
+        id: 8,
+        img: week9,
+        title: 'The Old Man That Lived In A Shoe',
+        week: 'Week 9'
+    },
+    {
+        id: 9,
+        img: week10,
+        title: 'Warm Yourself By The Fire Of Desire',
+        week: 'Week 10'
+    },
+    {
+        id: 10,
+        img: week11,
+        title: 'Winning Titles With No One To Share It With',
+        week: 'Week 11'
+    },
+    {
+        id: 11,
+        img: week12,
+        title: 'Dental Hygiene Is A Fundamental To A Happy Life',
+        week: 'Week 12'
+    },
+    {
+        id: 12,
+        img: week13,
+        title: 'Single Father',
+        week: 'Week 13'
+    },
+    {
+        id: 13,
+        img: week14,
+        title: 'Something To Take The Pain Away',
+        week: 'Week 14'
+    },
+    {
+        id: 14,
+        img: week15,
+        title: 'Single Father Part 2, Nurture vs Nature',
+        week: 'Week 15'
+    },
+    {
+        id: 15,
+        img: week16,
+        title: 'Oh Let The Sun Shine Upon Me, I Am A Traveler Of Both Time And Space',
+        week: 'Week 16'
+    },
+    {
+        id: 16,
+        img: week17,
+        title: 'I Broke My Nose In 7 Places, Little Bit Of A Flex',
+        week: 'Week 17'
+    },
+    {
+        id: 17,
+        img: week18,
+        title: "You Always Find When You Aren't Searching",
+        week: 'Week 18'
+    },
+]
+
